f04e8e1b14f79fc8aad985b8a87d9b3a755b6b2f
Cody Frank <cody@Codys-MacBook-Air.local>
2020-05-12 21:15:50 -0400

    creates face and value attributes

--- cards.rb
+++ cards.rb
@@ -1,10 +1,16 @@
 require 'pry'
 class Card 
-    attr_reader :value
-    def initialize(value)
+    attr_reader :value, :face
+    def initialize(face = nil)
+        @face = face
+        if @face.downcase == "jack" || @face.downcase == "queen" || @face.downcase == "king"
+            @value = 10
+        elsif @face == "ace"
+            @value = 1
+        else 
+            @value = @face.to_i
+        end
         @value = value
     end
 end
 
-card = Card.new(1)
-binding.pry
